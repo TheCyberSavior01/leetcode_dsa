ee57b6dc7be564a5f3b7ea3a6a4966157edeba6d
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-05-19 15:51:12 +0800

    added removeElement

--- main/java/org/limon/Main.java
+++ main/java/org/limon/Main.java
@@ -1,13 +1,11 @@
 package org.limon;
 
 import org.limon.Array.RemoveDuplicates;
+import org.limon.Array.RemoveElement;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //int[] nums = new int{0,0,1,1,1,2,2,3,3,4}
-        RemoveDuplicates removeDuplicates = new RemoveDuplicates();
-        //removeDuplicates.removeDuplicates();
-        System.out.println(removeDuplicates.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+        RemoveElement removeElement = new RemoveElement();
+        System.out.println(removeElement.removeElement(new int[]{0,1,2,2,3,0,4,2}, 2));
     }
 }
