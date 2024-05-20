60feb2e797f70e3c42ee7ea6635a28b5079b35fc
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-05-20 22:56:53 +0800

    added min stack

--- main/java/org/limon/Main.java
+++ main/java/org/limon/Main.java
@@ -2,14 +2,22 @@ package org.limon;
 
 import org.limon.Array.*;
 import org.limon.Stack.BaseBallGame;
+import org.limon.Stack.MinimumStack;
 import org.limon.Stack.ValidParentheses;
 
 public class Main {
     public static void main(String[] args) {
-        ValidParentheses validParentheses = new ValidParentheses();
-        String s = "[(])";
-        // [(])
-        // [(
-        System.out.println(validParentheses.isValid(s));
+        MinimumStack minimumStack = new MinimumStack();
+        minimumStack.push(-2);
+        minimumStack.push(-2);
+        minimumStack.push(-3);
+        minimumStack.push(-3);
+        System.out.println(minimumStack.getMin()); // return 0
+        minimumStack.pop();
+        //System.out.println(minimumStack.top());    // return 2
+        System.out.println(minimumStack.getMin());
+        // -2, -2, -3, -3
+        // -2, -2, -3, -3
+        // 3
     }
 }
