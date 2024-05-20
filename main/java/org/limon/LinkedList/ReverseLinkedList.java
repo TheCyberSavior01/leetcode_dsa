127960c7d3e72844826177fa852a1bb69111bc5d
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-05-20 23:46:24 +0800

    initialized Reverse Linked List

---
+++ main/java/org/limon/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,16 @@
+package org.limon.LinkedList;
+
+public class ReverseLinkedList {
+
+//    tail = null;
+//while (head.next != null):
+//    tail = head.next;
+//    tail.next = head;
+//    head.next = null;
+//
+
+
+
+// 0, 1, 2, 3
+//
+}
