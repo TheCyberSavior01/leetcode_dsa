ad927c7a6d16693ca758a4f5d32e1218ba2f10d0
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-05-21 16:05:51 +0800

    added reverse linked List

---
+++ main/java/org/limon/LinkedList/ListNode.java
@@ -0,0 +1,24 @@
+package org.limon.LinkedList;
+
+// Reverse Linked List
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = new ListNode();
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
--- main/java/org/limon/Main.java
+++ main/java/org/limon/Main.java
@@ -1,23 +1,13 @@
 package org.limon;
 
 import org.limon.Array.*;
+import org.limon.LinkedList.ListNode;
 import org.limon.Stack.BaseBallGame;
 import org.limon.Stack.MinimumStack;
 import org.limon.Stack.ValidParentheses;
 
 public class Main {
     public static void main(String[] args) {
-        MinimumStack minimumStack = new MinimumStack();
-        minimumStack.push(-2);
-        minimumStack.push(-2);
-        minimumStack.push(-3);
-        minimumStack.push(-3);
-        System.out.println(minimumStack.getMin()); // return 0
-        minimumStack.pop();
-        //System.out.println(minimumStack.top());    // return 2
-        System.out.println(minimumStack.getMin());
-        // -2, -2, -3, -3
-        // -2, -2, -3, -3
-        // 3
+
     }
 }
