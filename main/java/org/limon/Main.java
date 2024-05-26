40088999de112c42f5162025d5773430fc4f62ed
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-05-26 23:03:26 +0800

    added even numbers first solution

--- main/java/org/limon/Main.java
+++ main/java/org/limon/Main.java
@@ -1,5 +1,6 @@
 package org.limon;
 
+import org.limon.Array.EvenComesFirst;
 import org.limon.LinkedList.BrowserHistory;
 import org.limon.LinkedList.DesignLinkedList;
 import org.limon.LinkedList.MergeTwoSotredLinkedList;
@@ -9,10 +10,10 @@ import org.limon.Sorting.InsertionSort;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = new int[]{5,2,3,1};
-        InsertionSort insertionSort = new InsertionSort();
-        int[] ints = insertionSort.sortArray(nums);
-        for(int i : ints) {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
+        EvenComesFirst evenComesFirst = new EvenComesFirst();
+        int[] res = evenComesFirst.evenComesFirst(nums);
+        for(int i: res) {
             System.out.println(i);
         }
     }
