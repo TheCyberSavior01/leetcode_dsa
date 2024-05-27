039b6cc57c100cacd3a1d343cbca69282f36245a
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-05-27 20:07:22 +0800

    merge sort half

--- main/java/org/limon/Sorting/MergeSort.java
+++ main/java/org/limon/Sorting/MergeSort.java
@@ -1,4 +1,24 @@
 package org.limon.Sorting;
 
 public class MergeSort {
+    public int[] sortArray(int[] nums) {
+        int len = nums.length;
+        if (len < 2) return nums;
+        int mid = len / 2;
+        int[] left = new int[mid];
+        int[] right = new int[len - mid];
+
+        for (int i = 0; i < mid; i++) {
+            left[i] = nums[i];
+        }
+
+        for (int j = mid; j < len; j++) {
+            right[j - mid] = nums[j];
+        }
+
+    }
+}
+
+public int[] mergeSort(int[] array,  int length) {
+
 }
