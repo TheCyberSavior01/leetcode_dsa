2633de4738f3436d870044c9c1a2f0ef218107a3
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-05-29 22:25:37 +0800

    added quick sort impl

--- main/java/org/limon/playground/PlayGroundMain.java
+++ main/java/org/limon/playground/PlayGroundMain.java
@@ -4,11 +4,11 @@ public class PlayGroundMain {
     public static void main(String[] args) {
         int[] nums = new int[] {5, 1, 6, 2, 7, 3, 9};
 
-        mergeSortImpl mergeSortImpl = new mergeSortImpl();
-        int[] result = mergeSortImpl.mergeSort(nums);
-
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
+        QuickSortImpl quickSort = new QuickSortImpl();
+        int[] res = quickSort.quickSort(nums, 0, nums.length - 1);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
         }
+
     }
 }
---
+++ main/java/org/limon/playground/QuickSortImpl.java
@@ -0,0 +1,26 @@
+package org.limon.playground;
+
+public class QuickSortImpl {
+    public int[] quickSort(int[] arr, int start, int end) {
+        if (start >= end) return arr;
+
+        int j = start;
+        int pivot = arr[end];
+
+        for (int i = j; i < end; i++) {
+            if (arr[i] <= pivot) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+
+        arr[end] = arr[j];
+        pivot = arr[j];
+        quickSort(arr, start, j - 1);
+        quickSort(arr, j + 1, end);
+        return arr;
+
+    }
+}
