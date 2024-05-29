c01c5a4d4821514c9a3ba174217f1548e09d971f
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-05-29 22:16:44 +0800

    practiced merge sort

--- main/java/org/limon/playground/PlayGroundMain.java
+++ main/java/org/limon/playground/PlayGroundMain.java
@@ -2,6 +2,13 @@ package org.limon.playground;
 
 public class PlayGroundMain {
     public static void main(String[] args) {
+        int[] nums = new int[] {5, 1, 6, 2, 7, 3, 9};
 
+        mergeSortImpl mergeSortImpl = new mergeSortImpl();
+        int[] result = mergeSortImpl.mergeSort(nums);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
     }
 }
