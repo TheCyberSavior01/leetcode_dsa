03aadc40b5ca54a9a24c439b6dd020f6e5ed38e5
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-04 20:50:25 +0800

    tested and optimized best time to buy and sell stock

--- main/java/org/limon/Main.java
+++ main/java/org/limon/Main.java
@@ -3,15 +3,15 @@ package org.limon;
 
 import org.limon.Array.SecondMaximumValue;
 import org.limon.Searching.BinarySearch;
+import org.limon.SlidingWindow.BestTimeToBuyAndSellStock;
 
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         int[] arr = new int[] {2, 3, 6, 6, 11, 13, 1};
-        SecondMaximumValue secondMaximumValue = new SecondMaximumValue();
-        int res = secondMaximumValue.secondMaximumValue(arr);
-        System.out.println(res);
+        BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+        System.out.println(bestTimeToBuyAndSellStock.maxProfit(arr));
 
     }
 
