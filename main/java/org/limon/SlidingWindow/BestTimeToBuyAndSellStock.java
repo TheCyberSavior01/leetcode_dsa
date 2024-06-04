41f81a14f2dff70733baf0a740a215c6ce60a343
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-04 20:48:53 +0800

    implemented best time to buy and sell stock

--- main/java/org/limon/SlidingWindow/BestTimeToBuyAndSellStock.java
+++ main/java/org/limon/SlidingWindow/BestTimeToBuyAndSellStock.java
@@ -1,4 +1,23 @@
 package org.limon.SlidingWindow;
 
 public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int buy = 0;
+        int sell = 1;
+
+        while (sell < prices.length) {
+            if (prices[buy] < prices[sell]) {
+                int profit = prices[sell] - prices[buy];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+                sell++;
+            } else {
+                buy = sell;
+                sell++;
+            }
+        }
+
+        return maxProfit;
 }
