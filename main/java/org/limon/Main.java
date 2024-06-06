335756a1fb395d504d0b534a7ce59e738d2c075f
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-06 15:57:59 +0800

    started rotate array

--- main/java/org/limon/Main.java
+++ main/java/org/limon/Main.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[] {2, 3, 6, 6, 11, 13, 1};
+        int[] arr = new int[] {2, 3, 6, 1, 6, 11, 13, 1};
         BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
         System.out.println(bestTimeToBuyAndSellStock.maxProfit(arr));
 
