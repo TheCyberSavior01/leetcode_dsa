ada2ecc07e2ed499a5b8db87f63bafa2864b85dd
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-06 16:04:50 +0800

    brute force

--- main/java/org/limon/Array/RotateArray.java
+++ main/java/org/limon/Array/RotateArray.java
@@ -1,4 +1,14 @@
 package org.limon.Array;
 
 public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int j = 0;
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i + k > nums.length) {
+                nums[]
+            }
+        }
+    }
 }
