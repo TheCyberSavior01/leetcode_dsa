b87d5055c8b648094c1090055cfea9ee6166f4e4
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-07 16:46:15 +0800

    done with delete duplicates

--- main/java/org/limon/LinkedList/RemoveDuplicates.java
+++ main/java/org/limon/LinkedList/RemoveDuplicates.java
@@ -3,10 +3,32 @@ package org.limon.LinkedList;
 public class RemoveDuplicates {
     private class ListNode {
         int val;
-        MiddleOfLinkedList.ListNode next;
-        MiddleOfLinkedList.ListNode prev;
+        ListNode next;
+        ListNode prev;
         ListNode (int val) {
             this.val = val;
         }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode outer = head;
+
+        // 1 -> 2 -> 3 -> 4 -> 4
+
+        while (outer != null ) {
+            ListNode inner = outer;
+            while (inner != null && inner.next != null) {
+                if (inner.val == inner.next.val) {
+                   inner.next = inner.next.next;
+                } else {
+                    inner = inner.next;
+                }
+            }
+            outer = outer.next;
+        }
+
+        return head;
     }
 }
