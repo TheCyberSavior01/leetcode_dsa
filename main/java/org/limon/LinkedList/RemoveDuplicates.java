d5bc848eec782cf37dbea2e138369b55778e327e
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-07 16:36:32 +0800

    initiated remove duplicates

---
+++ main/java/org/limon/LinkedList/RemoveDuplicates.java
@@ -0,0 +1,12 @@
+package org.limon.LinkedList;
+
+public class RemoveDuplicates {
+    private class ListNode {
+        int val;
+        MiddleOfLinkedList.ListNode next;
+        MiddleOfLinkedList.ListNode prev;
+        ListNode (int val) {
+            this.val = val;
+        }
+    }
+}
