75ce0d4ae3fc9ae3429b579e2f00d619f98db6c5
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-07 15:18:47 +0800

    implememnted functionalitites for middle of the linked list

--- main/java/org/limon/LinkedList/MiddleOfLinkedList.java
+++ main/java/org/limon/LinkedList/MiddleOfLinkedList.java
@@ -1,4 +1,25 @@
 package org.limon.LinkedList;
 
 public class MiddleOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode curr = head;
+        int len = 0;
+
+        while (curr != null) {
+            len++;
+            curr = curr.next;
+        }
+
+        if (len == 1) return head;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
 }
