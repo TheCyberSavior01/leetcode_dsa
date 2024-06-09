be5525408689fb3ada17af2a9789f39adee3a482
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-09 17:16:19 +0800

    added playground

--- main/java/org/limon/Queue/QueueUsingStack.java
+++ main/java/org/limon/Queue/QueueUsingStack.java
@@ -1,10 +1,32 @@
 package org.limon.Queue;
 
+import java.util.Stack;
+
 public class QueueUsingStack {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
 
+    public QueueUsingStack() {
+        this.stack1 = new Stack<>();
+        this.stack2 = new Stack<>();
+    }
 
+    public void enQueue(int value) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
 
+        stack2.push(value);
 
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
 
-
+    public int dequeue() {
+        if (stack1.isEmpty()) return -1;
+        return stack1.pop();
+    }
 }
+
+
