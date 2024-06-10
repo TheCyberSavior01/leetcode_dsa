70ead4acf6b8eefba39bafd8126f9978ce5b58f3
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-10 16:56:46 +0800

    finished graph implementation

--- main/java/org/limon/Graphs/GraphImplementation.java
+++ main/java/org/limon/Graphs/GraphImplementation.java
@@ -1,4 +1,25 @@
 package org.limon.Graphs;
 
+import java.util.LinkedList;
+
 public class GraphImplementation {
+    private int vertices;
+    private LinkedList<Integer>[] adjacencyList;
+
+    public GraphImplementation(int vertices) {
+        this.vertices = vertices;
+        this.adjacencyList = new LinkedList[vertices];
+
+        for (int i = 0; i < vertices; i++) {
+            adjacencyList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge (int source, int destination) {
+        if (source < vertices && destination < vertices) {
+            this.adjacencyList[source].addLast(destination);
+        }
+    }
+
+
 }
