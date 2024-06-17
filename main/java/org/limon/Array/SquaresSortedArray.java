12b04e64d0b5b9d4c392f9f34d959b29f80b790b
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-18 00:32:05 +0800

    implemented and tested squares of sorted array

--- main/java/org/limon/Array/SquaresSortedArray.java
+++ main/java/org/limon/Array/SquaresSortedArray.java
@@ -1,4 +1,27 @@
 package org.limon.Array;
 
 public class SquaresSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int left = 0;
+        int right = n - 1;
+        int k = n - 1;
+
+        while (left <= right) {
+            int leftSqr = nums[left] * nums[left];
+            int rightSqr = nums[right] * nums[right];
+
+            if (leftSqr > rightSqr) {
+                res[k] = leftSqr;
+                left++;
+            } else {
+                res[k] = rightSqr;
+                right--;
+            }
+            k--;
+        }
+
+        return res;
+    }
 }
