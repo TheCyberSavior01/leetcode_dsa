838be10e3cf7fcbbf55e6473948908005f6413b2
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-17 15:29:56 +0800

    added size(), isEmpty() and enqueue() ops

--- main/java/org/limon/Queue/BasicQueue.java
+++ main/java/org/limon/Queue/BasicQueue.java
@@ -1,6 +1,11 @@
 package org.limon.Queue;
 
+import java.util.NoSuchElementException;
+
 public class BasicQueue {
+    private Node head;
+    private Node tail;
+    private int size;
 
     public static class Node{
         public int value;
@@ -11,4 +16,38 @@ public class BasicQueue {
             this.next = next;
         }
     }
+
+    public BasicQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void enqueue(int value) {
+        if (size == 0) {
+            head = new Node(value, null);
+            tail = head;
+        } else {
+            tail.next = new Node(value, null);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    public int dequeue() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        } else {
+
+        }
+    }
+
 }
