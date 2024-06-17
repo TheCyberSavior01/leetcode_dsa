79a887c6d437bc6a7894329b3f316102d8cf53de
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-17 15:25:45 +0800

    added node class

--- main/java/org/limon/Queue/BasicQueue.java
+++ main/java/org/limon/Queue/BasicQueue.java
@@ -1,4 +1,14 @@
 package org.limon.Queue;
 
 public class BasicQueue {
+
+    public static class Node{
+        public int value;
+        public Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
 }
