ae74ae772410d72e8de9f44f22a0f4c3a168bea3
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-25 17:56:11 +0800

    finished dfs iterative

--- main/java/org/limon/Graph/DFSIterative.java
+++ main/java/org/limon/Graph/DFSIterative.java
@@ -1,4 +1,21 @@
 package org.limon.Graph;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+
 public class DFSIterative {
+    public void DFS(HashMap<String, List<String>> graph, String source) {
+        Stack<String> stack = new Stack<>();
+        stack.push(source);
+
+        while (!stack.isEmpty()) {
+            String current = stack.pop();
+            System.out.println(current);
+            List<String> neighbors = graph.get(current);
+            for (String neighbor : neighbors) {
+                stack.push(neighbor);
+            }
+        }
+    }
 }
