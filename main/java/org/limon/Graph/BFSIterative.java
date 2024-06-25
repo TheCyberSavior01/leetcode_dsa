95a74b6c7bbcbe0a1dc472c2ea027d5ee08523ac
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-25 17:59:08 +0800

    finished bfs grapth

--- main/java/org/limon/Graph/BFSIterative.java
+++ main/java/org/limon/Graph/BFSIterative.java
@@ -1,4 +1,21 @@
 package org.limon.Graph;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BFSIterative {
+    public void BFS(HashMap<String, List<String>> graph, String source) {
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(source);
+        while (!queue.isEmpty()) {
+            String cur = queue.poll();
+            System.out.println(cur);
+            List<String> neighbors = graph.get(cur);
+            for (String neighbor : neighbors) {
+                queue.offer(neighbor);
+            }
+        }
+    }
 }
