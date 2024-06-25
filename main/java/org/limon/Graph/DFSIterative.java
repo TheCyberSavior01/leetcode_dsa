2b928230fc64aeaa157440280c0ed54ab33f32b3
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-06-25 18:13:14 +0800

    tested dfs

--- main/java/org/limon/Graph/DFSIterative.java
+++ main/java/org/limon/Graph/DFSIterative.java
@@ -1,19 +1,18 @@
 package org.limon.Graph;
 
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
 public class DFSIterative {
-    public void DFS(HashMap<String, List<String>> graph, String source) {
+    public void dfsGraph(Map<String, List<String>> graph, String source) {
         Stack<String> stack = new Stack<>();
         stack.push(source);
 
         while (!stack.isEmpty()) {
             String current = stack.pop();
             System.out.println(current);
-            List<String> neighbors = graph.get(current);
-            for (String neighbor : neighbors) {
+            for (String neighbor : graph.get(current)) {
                 stack.push(neighbor);
             }
         }
---
+++ main/java/org/limon/Graph/PlayGround.java
@@ -0,0 +1,22 @@
+package org.limon.Graph;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PlayGround {
+
+    public static void main(String[] args) {
+        Map<String, List<String>> graph = Map.of(
+                "a", List.of("c", "b"),
+                "b", List.of("d"),
+                "c", List.of("e"),
+                "d", List.of(),
+                "e", List.of()
+        );
+
+        DFSIterative dfsIterative = new DFSIterative();
+        dfsIterative.dfsGraph(graph, "a");
+
+    }
+}
